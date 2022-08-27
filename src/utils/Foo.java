cc2ec3b0a8629a9f6f3b3cef279164a23632f455
Jacob Schnitker <58757966+middspace@users.noreply.github.com>
2022-08-27 13:40:37 -0400

    added a variable for later use

---
+++ src/utils/Foo.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("Howdy");
+		System.out.println("hi from Github");
+		System.out.println("hi, jacob1 branch, from Eclipse");
+		System.out.println("hello worlds");
+
+	}
+
+}
